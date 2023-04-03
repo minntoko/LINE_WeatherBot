a76c447de6bdcc05dcc389d0c9b65265d3f19921
minntoko <minntoko0822@gmail.com>
2023-04-03 19:29:13 +0900

    add: 時間になったらメッセージを送信

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cron = require('cron');
 const line = require("@line/bot-sdk");
 const dotenv = require("dotenv");
 const app = express();
@@ -11,6 +12,11 @@ const config = {
 };
 
 const client = new line.Client(config);
+
+const message = {
+  type: 'text',
+  text: '明日の名古屋市の天気は晴れのち曇りです。最高気温23度、最低気温は12度です。'
+};
 
 const handleEvent = async (event) => {
   // メッセージじゃなかったら返信しない
@@ -31,6 +37,19 @@ app.post("/webhook", line.middleware(config), (req, res) => {
   res.sendStatus(200);
   events.map(handleEvent);
 });
+
+const job = new cron.CronJob('* 9 * * *', () => {
+  // 9時になったら、メッセージを送信する
+  client.pushMessage(process.env.USER_ID, message)
+    .then(() => {
+      console.log('9:00にメッセージを送信しました。');
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}, null, false, 'Asia/Tokyo');
+
+job.start();
 
 app.listen(port, () => {
   console.log("Node.js app listening at http://localhost:" + port);
