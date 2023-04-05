631d985f40bbc11824d772bb20bd082dd40defd2
minntoko <minntoko0822@gmail.com>
2023-04-05 18:50:24 +0900

    add: 正規表現や天気のAPI連携

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const cron = require('cron');
+const cron = require("cron");
 const line = require("@line/bot-sdk");
 const dotenv = require("dotenv");
+const request = require("request");
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
@@ -13,25 +14,64 @@ const config = {
 
 const client = new line.Client(config);
 
-const message = {
-  type: 'text',
-  text: '明日の名古屋市の天気は晴れのち曇りです。最高気温23度、最低気温は12度です。'
+const city = "Nagoya";
+
+const options = {
+  url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=ja&appid=${process.env.API_KEY}`,
+  method: "GET",
+  json: true,
 };
+
+let message = {};
+request(options, (error, res, body) => {
+  // 英語の天気表現を、日本語の天気表現に変換する
+  const weatherDict = {
+    Clouds: "曇り",
+    Sunny: "晴れ",
+    Rain: "雨",
+  };
+
+  function convertWeather(weather) {
+    const matchedKey = Object.keys(weatherDict).find((key) => {
+      const regex = new RegExp(key, "i");
+      return regex.test(weather);
+    });
+
+    const converted = weatherDict[matchedKey];
+    return converted ? converted : weather;
+  }
+
+  const temp_max = body.main.temp_max.toFixed(1);
+  const temp_min = body.main.temp_min.toFixed(1);
+  const temp = body.main.temp.toFixed(1);
+  const weather = convertWeather(body.weather[0].main);
+  message = {
+    type: "text",
+    text: `現在の${body.name}の天気は${weather}です。気温は${temp}°Cです。`,
+  };
+});
 
 const userIDs = [process.env.USER_ID1];
 
 const handleEvent = async (event) => {
+  const regex =
+    /(?=.*(?:天気|てんき|気温|きおん|予報|よほう))(?=.*(?:教えて|おしえて|出力|しゅつりょく))/;
   // メッセージじゃなかったら返信しない
-  if (event.type !== 'message' || event.message.type !== 'text') {
+  if (
+    event.type !== "message" ||
+    event.message.type !== "text" ||
+    !event.message.text.match(regex)
+  ) {
+    await client.replyMessage(event.replyToken, {
+      type: 'text',
+      text: `おうむ返しだぁぁ ${event.message.text}`
+    });
     return null;
   }
-  
+
   // ここで返信用メッセージを作成
-  await client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: event.message.text
-  })
-}
+  await client.replyMessage(event.replyToken, message);
+};
 
 app.post("/webhook", line.middleware(config), (req, res) => {
   const events = req.body.events;
@@ -40,16 +80,23 @@ app.post("/webhook", line.middleware(config), (req, res) => {
   events.map(handleEvent);
 });
 
-const job = new cron.CronJob('* 21 * * *', () => {
-  // 21時になったら、メッセージを送信する
-  client.multicast(userIDs, message)
-    .then(() => {
-      console.log('21:00にメッセージを送信しました。');
-    })
-    .catch((err) => {
-      console.error(err);
-    });
-}, null, false, 'Asia/Tokyo');
+const job = new cron.CronJob(
+  "* 21 * * *",
+  () => {
+    // 21時になったら、メッセージを送信する
+    client
+      .multicast(userIDs, message)
+      .then(() => {
+        console.log("21:00にメッセージを送信しました。");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  },
+  null,
+  false,
+  "Asia/Tokyo"
+);
 
 job.start();
 
