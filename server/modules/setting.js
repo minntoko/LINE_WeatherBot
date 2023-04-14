ccd92f4e90515a2162e95e6e9400ba553b39f4f2
minntoko <minntoko0822@gmail.com>
2023-04-14 21:36:07 +0900

    add: メッセージからクーロン式に変換する処理を追加

--- server/modules/setting.js
+++ server/modules/setting.js
@@ -3,25 +3,25 @@ const users = [
     userId: process.env.USER_ID1,
     cronExpression: ["0 0 9,21 * * Mon-Fri", "0 0 10,23 * * Sat-Sun"],
     region: "Nagoya",
-    enabled: true
+    enabled: true,
   },
   {
     userId: process.env.USER_ID1,
     cronExpression: ["0 59 23 * * Wed"],
     region: "Tokyo",
-    enabled: true
+    enabled: true,
   },
 ];
 
 // 地域変換テーブル
 const cityNames = {
-  "札幌": "Sapporo",
-  "仙台": "Sendai",
-  "東京": "Tokyo",
-  "名古屋": "Nagoya",
-  "大阪": "Osaka",
-  "福岡": "Fukuoka",
-  "那覇": "Naha",
+  札幌: "Sapporo",
+  仙台: "Sendai",
+  東京: "Tokyo",
+  名古屋: "Nagoya",
+  大阪: "Osaka",
+  福岡: "Fukuoka",
+  那覇: "Naha",
 };
 
 // 逆引き用
@@ -40,12 +40,12 @@ const convertCityName = (cityName) => {
 
   const converted = cityNames[matchedKey];
   return converted ? converted : null;
-}
+};
 
 // 地域を日本語に変換
 const reverseConvert = (cityName) => {
   return reversedCityNames[cityName];
-}
+};
 
 // ユーザ情報を更新
 const updateUser = (newUser) => {
@@ -54,32 +54,59 @@ const updateUser = (newUser) => {
   });
   const updatedUser = { ...targetUser, ...newUser };
   Object.assign(targetUser, updatedUser);
-  
+
   return updatedUser;
 };
 
 // ユーザを追加する処理
 const addUser = (userId) => {
-  // 新しいユーザーがすでに存在するかどうかを確認する
-  const isExistingUser = users.some(user => user.userId === userId);
-
+  const isExistingUser = users.some((user) => user.userId === userId);
   if (!isExistingUser) {
     const newUser = {
       userId: userId,
       cronExpression: [],
       region: null,
-      enabled: true
-    }
-    users.push(newUser)
+      enabled: true,
+    };
+    users.push(newUser);
     console.log(`User ${userId} has been added.`);
   } else {
     console.log(`User ${userId} already exists.`);
   }
 };
 
+// メッセージからクーロン式に変換する処理
+
+function createCronExpression(expression) {
+  const weekdays = ["日", "月", "火", "水", "木", "金", "土"];
+
+  const parts = expression
+    .replace(/曜日/g, "")
+    .replace(/毎週/g, "")
+    .replace(/分/g, "")
+    .replace(/と/g, ",")
+    .replace(/から/g, "-")
+    .replace(/時に通知して/g, "")
+    .split("の");
+
+  let minute = parts[1].split(/(?:時|:|：)/)[1];
+  let hour = parts[1].split(/(?:時|:|：)/)[0];
+
+  if (parts[0] === "平日") {
+    return `0 ${minute} ${hour} * * 1-5`;
+  } else if (parts[0] === "土日") {
+    return `0 ${minute} ${hour} * * 0,6`;
+  } else if (weekdays.includes(parts[0])) {
+    const dayOfWeek = weekdays.indexOf(parts[0]);
+    return `0 ${minute} ${hour} * * ${dayOfWeek}`;
+  }
+
+  throw new Error("Invalid expression");
+}
+
 module.exports = {
   users: users,
   updateUser: updateUser,
   convertCityName: convertCityName,
-  reverseConvert: reverseConvert
+  reverseConvert: reverseConvert,
 };
