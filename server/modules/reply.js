ba72d5461ed86b1e1fde79e4c5476124403f58de
minntoko <minntoko0822@gmail.com>
2023-04-30 17:14:45 +0900

    add: 天気のリプライメッセージを追加

---
+++ server/modules/reply.js
@@ -0,0 +1,62 @@
+const weatherReplyItems = [
+  {
+    type: "action",
+    action: {
+      type: "message",
+      label: "札幌の天気",
+      text: "札幌の天気を教えて",
+    },
+  },
+  {
+    type: "action",
+    action: {
+      type: "message",
+      label: "仙台の天気",
+      text: "仙台の天気を教えて",
+    },
+  },
+  {
+    type: "action",
+    action: {
+      type: "message",
+      label: "東京の天気",
+      text: "東京の天気を教えて",
+    },
+  },
+  {
+    type: "action",
+    action: {
+      type: "message",
+      label: "名古屋の天気",
+      text: "名古屋の天気を教えて",
+    },
+  },
+  {
+    type: "action",
+    action: {
+      type: "message",
+      label: "大阪の天気",
+      text: "大阪の天気を教えて",
+    },
+  },
+  {
+    type: "action",
+    action: {
+      type: "message",
+      label: "福岡の天気",
+      text: "福岡の天気を教えて",
+    },
+  },
+  {
+    type: "action",
+    action: {
+      type: "message",
+      label: "那覇の天気",
+      text: "那覇の天気を教えて",
+    },
+  },
+];
+ 
+module.exports = {
+  weatherReplyItems,
+};
