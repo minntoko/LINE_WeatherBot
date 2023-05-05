885eba1b739a0572c69c6fc42e7e003effcc3ed6
minntoko <minntoko0822@gmail.com>
2023-05-05 23:50:41 +0900

    fix: 通知が動くように修正

--- server/modules/cron.js
+++ server/modules/cron.js
@@ -7,42 +7,57 @@ const periodic = () => {
   users.forEach((user) => {
     if (user.enabled) {
       const cronExpressions = user.cronExpression;
+      const jobs = [];
       cronExpressions.forEach((cronExpression) => {
         const job = new cron.CronJob(
           cronExpression,
           async () => {
             const message = await getWeather(user.region || "Nagoya");
             const flexMessage = await getWeatherFlex(user.region || "Nagoya");
-            client
-              .pushMessage(user.userId, [
-                {
-                  type: "text",
-                  text: `${reverseConvert(user.region || "Nagoya")}の天気をお伝えします。`,
-                },
-                {
-                  type: "flex",
-                  altText: message,
-                  contents: flexMessage,
-                },
-              ])
-              .then(() => {
-                console.log(
-                  `ユーザーID ${user.userId} にメッセージを送信しました。`
-                );
-              })
-              .catch((err) => {
-                console.error(err);
-              });
+            try {
+              await sendWeatherMessage(
+                user.userId,
+                message,
+                flexMessage,
+                user.region
+              );
+              console.log(
+                `ユーザーID ${user.userId} にメッセージを送信しました。`
+              );
+            } catch (err) {
+              console.error(err);
+            }
           },
           null,
           false,
           "Asia/Tokyo"
         );
+        jobs.push(job);
+      });
+      jobs.forEach((job) => {
         job.start();
       });
     }
-    return null;
   });
+
+  async function sendWeatherMessage(
+    userId,
+    message,
+    flexMessage,
+    region = "Nagoya"
+  ) {
+    await client.pushMessage(userId, [
+      {
+        type: "text",
+        text: `${reverseConvert(region || "Nagoya")}の天気をお伝えします。`,
+      },
+      {
+        type: "flex",
+        altText: message,
+        contents: flexMessage,
+      },
+    ]);
+  }
 };
 
 module.exports = { periodic: periodic };
